8eb3e8a6e3085e86778ff987e8dc84b836ded865
Raks-Javac <rufaikudus2014@gmail.com>
2025-08-11 02:37:04 +0100

    Add nginx proxy to serve React app directly at /web path
    - Updated Dockerfile to install nginx and supervisor
    - Added nginx config to serve React build files at /web
    - Configured supervisor to run both nginx and Django
    - Removed Django React serving logic from urls.py
    - React app will now be served directly by nginx

--- backend/ecommerce_backend/urls.py
+++ backend/ecommerce_backend/urls.py
@@ -1,45 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import FileResponse, Http404
-from django.views.generic import TemplateView
-import os
-
-class ReactAppView(TemplateView):
-    """Serve React app using Django's template system"""
-    template_name = 'index.html'
-    
-    def get(self, request, *args, **kwargs):
-        try:
-            # Try to serve the actual React index.html from static files
-            index_path = os.path.join(settings.STATIC_ROOT, 'index.html')
-            if os.path.exists(index_path):
-                return FileResponse(open(index_path, 'rb'), content_type='text/html')
-            else:
-                # Fallback to a basic HTML page that loads React
-                from django.http import HttpResponse
-                return HttpResponse(f'''
-                <!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="utf-8" />
-                    <link rel="icon" href="{settings.STATIC_URL}favicon.ico" />
-                    <meta name="viewport" content="width=device-width,initial-scale=1" />
-                    <meta name="theme-color" content="#000000" />
-                    <meta name="description" content="E-Commerce Application" />
-                    <title>E-Commerce App</title>
-                    <link href="{settings.STATIC_URL}css/main.92aa7f4b.css" rel="stylesheet">
-                </head>
-                <body>
-                    <noscript>You need to enable JavaScript to run this app.</noscript>
-                    <div id="root"></div>
-                    <script src="{settings.STATIC_URL}js/main.d79b7a57.js"></script>
-                </body>
-                </html>
-                ''', content_type='text/html')
-        except Exception as e:
-            raise Http404(f"React app not found: {str(e)}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -49,12 +11,6 @@ urlpatterns = [
     path('api/orders/', include('orders.urls')),
 ]
 
-# Serve static and media files normally
+# Serve static and media files
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# Serve React app under /web/ path
-urlpatterns += [
-    path('web/', ReactAppView.as_view(), name='react_app_root'),
-    re_path(r'^web/(?P<path>.*)$', ReactAppView.as_view(), name='react_app'),
-]
