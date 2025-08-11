275b8c5a8bdaa6be1ff9b5d6bdfe4a4d55851922
Raks-Javac <rufaikudus2014@gmail.com>
2025-08-11 02:21:39 +0100

    Fix React app serving at /web/ path - simplified approach using class-based view

--- backend/ecommerce_backend/urls.py
+++ backend/ecommerce_backend/urls.py
@@ -3,24 +3,43 @@ from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import FileResponse, Http404
+from django.views.generic import TemplateView
 import os
 
-def serve_react_app(request, path=''):
-    """Serve React app for /web/ routes"""
-    try:
-        if path == '' or path == 'index.html':
-            # Serve index.html for root web requests
-            return FileResponse(open(os.path.join(settings.STATIC_ROOT, 'index.html'), 'rb'))
-        else:
-            # Try to serve the requested static file
-            file_path = os.path.join(settings.STATIC_ROOT, path)
-            if os.path.exists(file_path):
-                return FileResponse(open(file_path, 'rb'))
+class ReactAppView(TemplateView):
+    """Serve React app using Django's template system"""
+    template_name = 'index.html'
+    
+    def get(self, request, *args, **kwargs):
+        try:
+            # Try to serve the actual React index.html from static files
+            index_path = os.path.join(settings.STATIC_ROOT, 'index.html')
+            if os.path.exists(index_path):
+                return FileResponse(open(index_path, 'rb'), content_type='text/html')
             else:
-                # If file doesn't exist, serve index.html (for React routing)
-                return FileResponse(open(os.path.join(settings.STATIC_ROOT, 'index.html'), 'rb'))
-    except:
-        raise Http404("File not found")
+                # Fallback to a basic HTML page that loads React
+                from django.http import HttpResponse
+                return HttpResponse(f'''
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8" />
+                    <link rel="icon" href="{settings.STATIC_URL}favicon.ico" />
+                    <meta name="viewport" content="width=device-width,initial-scale=1" />
+                    <meta name="theme-color" content="#000000" />
+                    <meta name="description" content="E-Commerce Application" />
+                    <title>E-Commerce App</title>
+                    <link href="{settings.STATIC_URL}css/main.92aa7f4b.css" rel="stylesheet">
+                </head>
+                <body>
+                    <noscript>You need to enable JavaScript to run this app.</noscript>
+                    <div id="root"></div>
+                    <script src="{settings.STATIC_URL}js/main.d79b7a57.js"></script>
+                </body>
+                </html>
+                ''', content_type='text/html')
+        except Exception as e:
+            raise Http404(f"React app not found: {str(e)}")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,6 +55,6 @@ urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Serve React app under /web/ path
 urlpatterns += [
-    path('web/', serve_react_app, name='react_app_root'),
-    re_path(r'^web/(?P<path>.*)$', serve_react_app, name='react_app'),
+    path('web/', ReactAppView.as_view(), name='react_app_root'),
+    re_path(r'^web/(?P<path>.*)$', ReactAppView.as_view(), name='react_app'),
 ]
